48ef8aab142f37e0b3655f26ddd9d0564baef4bc
kenjimaeda54 <kenjimaedafamily@gmail.com>
2026-03-05 19:06:58 -0300

    Initial Commit

---
+++ control_student/control_studentApp.swift
@@ -0,0 +1,17 @@
+//
+//  control_studentApp.swift
+//  control_student
+//
+//  Created by kenjimaeda on 05/03/26.
+//
+
+import SwiftUI
+
+@main
+struct control_studentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
